1e37a96f9ce287e0f4f27feac5112efd1c398147
Maciej Gajdzica <mgajdzica@gmail.com>
2017-08-12 12:27:13 +0200

    tx master: add addr argument check to pass tests.
    Phase green.

--- src/comm/comm_master.c
+++ src/comm/comm_master.c
@@ -12,5 +12,10 @@ int32_t comm_master_tx(comm_addr_t addr, uint8_t *buf)
 		return COMM_ERR_INVALID;
 	}
 
+	if ((0x0000 == addr) || (0xFFFF == addr))
+	{
+		return COMM_ERR_INVALID;
+	}
+
 	return COMM_ERR_OK;
 }
