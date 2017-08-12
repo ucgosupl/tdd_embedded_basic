e6a378aa0c5622d34b82a174c559972eb177bb2d
Maciej Gajdzica <mgajdzica@gmail.com>
2017-08-12 13:14:56 +0200

    master tx: add implementation of seq nr.
    Phase green.
    When implementing one important design question emerged.

--- src/comm/comm_master.c
+++ src/comm/comm_master.c
@@ -3,10 +3,11 @@
 #include <string.h>
 
 #include "comm_master.h"
+#include "comm_table.h"
 
 int32_t comm_master_tx(comm_addr_t addr, uint8_t *buf)
 {
-	(void) addr;
+	comm_seq_t seq;
 
 	if (NULL == buf)
 	{
@@ -19,6 +20,10 @@ int32_t comm_master_tx(comm_addr_t addr, uint8_t *buf)
 	}
 
 	memcpy(&buf[0], &addr, sizeof(comm_addr_t));
+
+	seq = comm_table_seq_get(addr);
+	/* What if address provided is not in conn table and seq is invalid? */
+	memcpy(&buf[2], &seq, sizeof(comm_seq_t));
 
 	return COMM_ERR_OK;
 }
