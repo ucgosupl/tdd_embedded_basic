68fc0adf263b58cb45ee5da978012cd5ebd794ac
Maciej Gajdzica <mgajdzica@gmail.com>
2017-08-12 12:01:28 +0200

    master tx: update implementation of conn_master_tx to pass all tests.
    Phase green.
    Added just enough code to pass all tests, not more.

--- src/comm/comm_master.c
+++ src/comm/comm_master.c
@@ -1,11 +1,16 @@
 #include <stdint.h>
+#include <stddef.h>
 
 #include "comm_master.h"
 
 int32_t comm_master_tx(uint16_t addr, uint8_t *buf)
 {
 	(void) addr;
-	(void) buf;
 
-	return -1;
+	if (NULL == buf)
+	{
+		return -1;
+	}
+
+	return 0;
 }
--- unit_test/comm_master/comm_master_test.c
+++ unit_test/comm_master/comm_master_test.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stddef.h>
 
 #include "unity/fixture/unity_fixture.h"
 
